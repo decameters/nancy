b66db32536dd0d3d6ae3817a62571baacb44f64c
liz elton <eeeqqqeee@gmail.com>
2017-12-22 13:44:31 -0600

    updated bootstrap in vendors

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -14,7 +14,7 @@ router.post('/',
 // Handle index file separately
 // Also catches any other request not explicitly matched elsewhere
 router.get('/', function(req, res) {
-  console.log('request for index');
+  // console.log('request for index');
   res.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
