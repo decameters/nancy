75f965c2fe9a865c7ce3ccd43395e51bf6597522
liz elton <eeeqqqeee@gmail.com>
2018-01-12 09:57:20 -0600

    updated database

--- database.sql
+++ database.sql
@@ -1,125 +1,97 @@
 CREATE DATABASE nancy;
 
-CREATE TABLE "users" (
+CREATE TABLE "users"
+(
 	"id" serial NOT NULL,
 	"username" serial NOT NULL UNIQUE,
 	"password" serial NOT NULL UNIQUE,
-	"first_name" varchar NOT NULL,
-	"last_name" varchar NOT NULL,
-	"phone" int(11) NOT NULL,
+	"name" varchar NOT NULL,
+	"phone" bigint NOT NULL,
 	CONSTRAINT users_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "itinerary_item" (
+CREATE TABLE "itinerary_item"
+(
 	"id" serial NOT NULL,
-	"tripname_id" int NOT NULL,
 	"date" DATE NOT NULL,
-	"location_id" int NOT NULL,
-	"contact_id" int NOT NULL,
+	"contact_id" int,
+	"drivetime" varchar,
+	"city_state" varchar NOT NULL,
+	"destination" varchar,
+	"address" varchar,
+	"trip_id" int NOT NULL,
 	CONSTRAINT itinerary_item_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "tripnames" (
+CREATE TABLE "tripnames"
+(
 	"id" serial NOT NULL,
 	"name" varchar NOT NULL UNIQUE,
 	"link" varchar,
-	"created_id" varchar NOT NULL,
+	"created_id" int NOT NULL,
 	CONSTRAINT tripnames_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "locations" (
+CREATE TABLE "locations"
+(
 	"id" serial NOT NULL,
 	"name" varchar NOT NULL UNIQUE,
 	"address" varchar,
-	"phone" int(11),
+	"phone" bigint NOT NULL,
+	"city_state" varchar,
 	CONSTRAINT locations_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "contacts" (
+CREATE TABLE "contacts"
+(
 	"id" serial NOT NULL,
-	"first_name" varchar NOT NULL,
-	"last_name" varchar NOT NULL,
-	"email" varchar NOT NULL,
-	"phone" int(11) NOT NULL,
+	"person" varchar NOT NULL,
+	"email" varchar,
+	"phone" bigint,
+	"created_id" int,
 	CONSTRAINT contacts_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "list_items" (
+CREATE TABLE "list_items"
+(
 	"id" serial NOT NULL,
 	"name_id" int NOT NULL,
 	"item" varchar NOT NULL,
 	"quantity" int,
 	"is_packed" BOOLEAN NOT NULL,
 	CONSTRAINT list_items_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "listnames" (
+CREATE TABLE "listnames"
+(
 	"id" serial NOT NULL,
 	"name" varchar NOT NULL UNIQUE,
-	"created_id" int NOT NULL UNIQUE,
+	"created_id" int,
 	CONSTRAINT listnames_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "trips_shared_with" (
+CREATE TABLE "trips_shared_with"
+(
 	"id" serial NOT NULL,
 	"user_id" int NOT NULL,
 	"tripnames_id" int NOT NULL,
 	CONSTRAINT trips_shared_with_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "lists_shared_with" (
+CREATE TABLE "lists_shared_with"
+(
 	"id" serial NOT NULL,
 	"user_id" serial NOT NULL,
 	"listnames_id" serial NOT NULL,
 	CONSTRAINT lists_shared_with_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-
-ALTER TABLE "itinerary_item" ADD CONSTRAINT "itinerary_item_fk0" FOREIGN KEY ("tripname_id") REFERENCES "tripnames"("id");
-ALTER TABLE "itinerary_item" ADD CONSTRAINT "itinerary_item_fk1" FOREIGN KEY ("location_id") REFERENCES "locations"("id");
+ALTER TABLE "public"."itinerary_item" ADD CONSTRAINT "itinerary_item_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "public"."tripnames" ("id") ON DELETE CASCADE;
 ALTER TABLE "itinerary_item" ADD CONSTRAINT "itinerary_item_fk2" FOREIGN KEY ("contact_id") REFERENCES "contacts"("id");
 
 ALTER TABLE "tripnames" ADD CONSTRAINT "tripnames_fk0" FOREIGN KEY ("created_id") REFERENCES "users"("id");
 
-
-
-ALTER TABLE "list_items" ADD CONSTRAINT "list_items_fk0" FOREIGN KEY ("name_id") REFERENCES "listnames"("id");
+ALTER TABLE "public"."list_items" ADD CONSTRAINT "list_items_name_id_fkey" FOREIGN KEY ("name_id") REFERENCES "public"."listnames" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "listnames" ADD CONSTRAINT "listnames_fk0" FOREIGN KEY ("created_id") REFERENCES "users"("id");
 
@@ -128,4 +100,10 @@ ALTER TABLE "trips_shared_with" ADD CONSTRAINT "trips_shared_with_fk1" FOREIGN K
 
 ALTER TABLE "lists_shared_with" ADD CONSTRAINT "lists_shared_with_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 ALTER TABLE "lists_shared_with" ADD CONSTRAINT "lists_shared_with_fk1" FOREIGN KEY ("listnames_id") REFERENCES "listnames"("id");
+
+ALTER TABLE "contacts" ADD CONSTRAINT "contacts_fk0" FOREIGN KEY ("created_id") REFERENCES "users"("id");
+
+
+
+
 
